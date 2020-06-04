34534be660eaeeda3241fede3a1d490133621f4d
YoshimatsuSaito <middle158725071@gmail.com>
2020-06-04 15:34:47 +0900

    added step2 to 3

--- Ch1.py
+++ Ch1.py
@@ -1,3 +1,4 @@
+#step1
 class Variable:
     def __init__(self, data):
         self.data=data
@@ -14,3 +15,48 @@ class Function:
         output = Variable(y)
         return output
 
+x = Variable(np.array(10))
+f = Function()
+y = f(x)
+
+print(type(y))
+print(y.data)
+
+#step2
+class Function:
+    def __call__(self, input):
+        x=input.data
+        y=self.forward(x)
+        output=Variable(y)
+        return output
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x):
+        return x**2
+
+x=Variable(np.array(10))
+f=Square()
+y=f(x)
+print(type(y))
+print(y.data)
+
+#step3
+class Exp(Function):
+    def forward(self,x):
+        return np.exp(x)
+
+A=Square()
+B=Exp()
+C=Square()
+
+x=Variable(np.array(0.5))
+a=A(x)
+b=B(a)
+y=C(b)
+print(y.data)
+
+#step4
