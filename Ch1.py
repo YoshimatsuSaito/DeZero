4a92fc22330d97fe4cdb8df866e9fdd9cbd3b6d4
YoshimatsuSaito <middle158725071@gmail.com>
2020-06-04 18:36:58 +0900

    Update Ch1.py

--- Ch1.py
+++ Ch1.py
@@ -268,3 +268,120 @@ y=C(b)
 y.grad=np.array(1)
 y.backward()
 print(x.grad)
+
+
+#step9
+def square(x):
+    f=Square()
+    return f(x)
+
+def exp(x):
+    f=Exp()
+    return f(x)
+
+x=Variable(np.array(0.5))
+a=square(x)
+b=exp(a)
+y=square(b)
+
+
+x=Variable(np.array(0.5))
+y=square(exp(square(x)))
+
+y.grad=np.array(1.0)
+y.backward()
+print(x.grad)
+
+
+class Variable:
+    def __init__(self,data):
+        self.data=data
+        self.grad=None
+        self.creator=None
+
+    def set_creator(self,func):
+        self.creator=func
+
+    def backward(self):
+        if self.grad is None:
+            self.grad=np.ones_like(self.data)
+
+        funcs=[self.creator]
+        while funcs:
+            f=funcs.pop()
+            x,y=f.input, f.output
+            x.grad=f.backward(y.grad)
+
+            if x.creator is not None:
+                funcs.append(x.creator)
+
+x=Variable(np.array(0.5))
+y=square(exp(square(x)))
+y.backward()
+print(x.grad)
+
+class Variable:
+    def __init__(self,data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{} is not supported'.format(type(data)))
+
+        self.data=data
+        self.grad=None
+        self.creator=None
+
+    def set_creator(self,func):
+        self.creator=func
+
+    def backward(self):
+        if self.grad is None:
+            self.grad=np.ones_like(self.data)
+
+        funcs=[self.creator]
+        while funcs:
+            f=funcs.pop()
+            x,y=f.input, f.output
+            x.grad=f.backward(y.grad)
+
+            if x.creator is not None:
+                funcs.append(x.creator)
+
+def as_array(x):
+    if np.isscaler(x):
+        return np.array(x)
+    return x
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(as_array(y))
+        output.set_creator(self)  # Set parent(function)
+        self.input = input
+        self.output = output  # Set output
+        return output
+
+#step10
+import unittest
+class SquareTest(unittest.TestCase):
+    def test_forward(self):
+        x=Variable(np.array(2.0))
+        y=square(x)
+        expected=np.array(4.0)
+        self.assertEqual(y.data,expected)
+    
+    def test_backward(self):
+        x=Variable(np.array(3.0))
+        y=square(x)
+        y.backward()
+        expected=np.array(6.0)
+        self.assertEqual(x.grad, expected)
+
+    def test_gradient_check(self):
+        x=Variable(np.random.rand(1))
+        y=square(x)
+        y.backward()
+        num_grad=numerical_diff(square, x)
+        flg=np.allclose(x.grad, num_grad)
+        self.assertTrue(flg)
+        
