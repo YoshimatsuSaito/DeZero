facb2d99e272c8299d3a30609c401d23e55104bf
YoshimatsuSaito <middle158725071@gmail.com>
2020-06-04 15:07:26 +0900

    added Function class that calculates square

--- Ch1.py
+++ Ch1.py
@@ -6,3 +6,11 @@ import numpy as np
 data=np.array(1.0)
 x=Variable(data)
 print(x.data)
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = x**2
+        output = Variable(y)
+        return output
+
