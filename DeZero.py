9e641725dd03537fc1836e875441b62f44d9b34b
YoshimatsuSaito <middle158725071@gmail.com>
2020-06-08 11:43:52 +0900

    'step22'

--- DeZero.py
+++ DeZero.py
@@ -169,6 +169,47 @@ class Mul(Function):
         x0,x1=self.inputs[0].data, self.inputs[1].data
         return gy*x1, gy*x0
 
+class Neg(Function):
+    def forward(self,x):
+        return -x
+    
+    def backward(self,gy):
+        return -gy
+
+class Sub(Function):
+    def forward(self, x0, x1):
+        y=x0-x1
+        return y
+    
+    def backward(self, gy):
+        return gy, -gy
+
+class Div(Function):
+    def forward(self,x0,x1):
+        y=x0/x1
+        return y
+
+    def backward(self,gy):
+        x0,x1=self.inputs[0].data, self.inputs[1].data
+        gx0=gy/x1
+        gx1=gy*(-x0/x1**2)
+        return gx0,gx1
+
+class Pow(Function):
+    def __init__(self,c):
+        self.c=c
+
+    def forward(self,x):
+        y=x**self.c
+        return y
+    
+    def backward(self,gy):
+        x=self.inputs[0].data
+        c=self.c
+        gx=c*x**(c-1)*gy
+        return gx
+
+
 def square(x): #util function
     f=Square()
     return f(x)
@@ -185,6 +226,28 @@ def mul(x0,x1):
     x1=as_array(x1)
     return Mul()(x0,x1)
 
+def neg(x):
+    return Neg()(x)
+
+
+def sub(x0,x1):
+    x1=as_array(x1)
+    return Sub()(x0,x1)
+
+def rsub(x0,x1):
+    x1=as_array(x1)
+    return Sub()(x1,x0)
+
+def div(x0,x1):
+    x1=as_array(x1)
+    return Div()(x0,x1)
+
+def rdiv(x0,x1):
+    x1=as_array(x1)
+    return Div()(x1,x0)
+
+def pow(x,c):
+    return Pow(c)(x)
 
 #whether doing derivative or not
 class Config:
@@ -216,3 +279,9 @@ Variable.__mul__=mul
 Variable.__rmul__=mul
 Variable.__add__=add
 Variable.__radd__=add
+Variable.__neg__=neg
+Variable.__sub__=sub
+Variable.__rsub__=rsub
+Variable.__truediv__=div
+Variable.__rtruediv__=div
+Variable.__pow__=pow
