fcf7234badb76a9a84a472a4860c763e9b5f85f7
YoshimatsuSaito <middle158725071@gmail.com>
2020-06-09 13:35:17 +0900

    Step 39

--- dezero/functions.py
+++ dezero/functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 from dezero.core import Function, as_variable
+from dezero import utils
 
 class Sin(Function):
     def forward(self, x):
@@ -77,3 +78,20 @@ class Transpose(Function):
 def transpose(x):
     return Transpose()(x)
 
+class Sum(Function):
+    def __init__(self, axis, keepdims):
+        self.axis = axis
+        self.keepdims = keepdims
+
+    def forward(self, x):
+        self.x_shape = x.x_shape
+        y = x.sum(axis=self.axis, keepdims=self.keepdims)
+        return y
+    
+    def backward(self, gy):
+        gy = utils.reshape_sum_backward(gy, self.x_shape, self.axis, self.keepdims)
+        gx = broadcast_to(gy, self.x_shape)
+        return gx
+
+def sum(x, axis=None, keepdims=False):
+    return Sum(axis, keepdims)(x)
